6f24f922d8b3d96e2062e7fafa081ec55d4a1407
creme <creme@envs.net>
2019-09-23 11:18:09 +0200

    blockqoute fix

--- fhost.py
+++ fhost.py
@@ -372,7 +372,7 @@ def fhost():
 <pre><blockquote>
 <h2>envs.sh &#124; THE NULL POINTER</h2>
 <h4>file hosting and URL shortening service.</h4>
-<blockquote>
+</blockquote>
 <h3>USAGE</h3>
 HTTP POST files here:
     <code>curl -F'file=&#64;yourfile.png' {0}</code>
